740254ae8f2a1f9a4f96d01da64b567bc2f9ec74
Patrik <lktpriv@gmail.com>
2023-05-23 11:05:29 +0200

    added followbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./layout/Sidebar";
+import FollowBar from "./layout/FollowBar";
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -9,10 +10,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
-          <Sidebar/>
+          <Sidebar />
           <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
             {children}
           </div>
+          <FollowBar />
         </div>
       </div>
     </div>
